392a65a2b3e4a55476e604e81a361bb7c06809ec
Mutalib SH. Gozalov <mutalibshg@code.local>
2023-05-02 12:56:45 +0400

    RabbitMQ

---
+++ RabbitMQ_Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ;
+
+using RabbitMQ;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://svnwklcy:BIqAmhaOvIpke8pI7ntccKUbzX_xuYBD@shark.rmq.cloudamqp.com/svnwklcy");
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "queue", durable: false, exclusive: false, autoDelete: true);
+
+        EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(channel);
+        channel.BasicConsume(queue: "queue", autoAck: true, consumer: eventingBasicConsumer);
+        eventingBasicConsumer.Received += (sender, e) =>
+        {
+            var data = Encoding.UTF8.GetString(e.Body.Span);
+            Console.WriteLine(data);
+        };
+    }
+}
+Console.ReadLine();
---
+++ RabbitMQ_Publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ;
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://svnwklcy:BIqAmhaOvIpke8pI7ntccKUbzX_xuYBD@shark.rmq.cloudamqp.com/svnwklcy");
+
+using(var connection = factory.CreateConnection())
+{
+    using(var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "queue", durable: false, exclusive: false, autoDelete: true);
+
+        var byteData = Encoding.UTF8.GetBytes("JFLSKJFLSJLDFSKJ SLKJFLSKFJ SFSF");
+       for(int i = 0; i < 100; i++)
+        {
+            channel.BasicPublish(exchange: "", routingKey: "queue", body: byteData);
+            Console.WriteLine("sent");
+        }
+    }
+}
